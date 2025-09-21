7cd0ea8832471904aea06d14162e79f4c322b71a
samarthlawania <samarthlawania2002@gmail.com>
2025-09-21 13:05:55 +0530

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,22 +3,27 @@ require("dotenv").config();
 
 const isDev = process.env.NODE_ENV === "development";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: process.env.NODE_ENV === 'production' ? {
-        require: true,
-        rejectUnauthorized: false
-      } : false
-    },
-    logging: false,
-  }
-);
+const sequelize = process.env.DATABASE_URL 
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || "127.0.0.1",
+        port: process.env.DB_PORT || 5432,
+        dialect: "postgres",
+        logging: false,
+      }
+    );
 
 module.exports = sequelize;
